79eb8bd5dbad312d11bdc16a3808db2f19c57fb7
ghostbuster91 <kghost0@gmail.com>
2021-04-12 22:11:12 +0200

    Adhere to single naming convention

--- core/src/test/scala/io/github/ghostbuster91/sttp/client3/FlattenerTest.scala
+++ core/src/test/scala/io/github/ghostbuster91/sttp/client3/FlattenerTest.scala
@@ -7,29 +7,24 @@ import io.github.ghostbuster91.sttp.client3.openapi.OpenApiLoader
 
 object FlattenerTest extends TestSuite {
   val tests = Tests {
-    "component-schemas-unnest" - test()
-    "component-req-body-unnest" - test()
-    "flattened-enum-left-untouched" - test()
+    "component_schemas_unnest" - test()
+    "component_req_body_unnest" - test()
+    "flattened_enum_left_untouched" - test()
     "header" - test()
     "path" - test()
     "query" - test()
-    "request-body" - test()
-    "response-body" - test()
+    "request_body" - test()
+    "response_body" - test()
   }
 
   def test()(implicit testPath: utest.framework.TestPath) = {
-    val expected = load(s"${testPath.value.mkString("/")}-expected.yaml")
+    val expected = load(s"${testPath.value.mkString("/")}_expected.yaml")
     val input = load(s"${testPath.value.mkString("/")}.yaml")
     val actual = Yaml.pretty(OpenApiLoader.load(input).unsafe)
     assert(parser.parse(actual) == parser.parse(expected))
   }
 
   private def load(fileName: String): String =
-    scala.io.Source
-      .fromInputStream(
-        getClass.getResourceAsStream(s"/enum-flattener/$fileName")
-      )
-      .getLines()
-      .mkString("\n")
+    FileLoader.loadFile("enum_flattener", fileName)
 
 }
