a0d1db3cdda6518caf66b2f44eacab54693fcd3c
ghostbuster91 <kghost0@gmail.com>
2021-05-04 12:06:31 +0200

    Add e2e test with pet store (broken)

---
+++ core/src/test/scala/io/github/ghostbuster91/sttp/client3/PetStoreTest.scala
@@ -0,0 +1,32 @@
+package io.github.ghostbuster91.sttp.client3
+
+import io.github.ghostbuster91.sttp.client3.GeneratorTest.StringShouldCompileHelper
+import utest._
+
+object PetStoreTest extends TestSuite {
+
+  val tests = Tests {
+//    "pet_store" - testCompile() //FIX ME
+  }
+
+  def testCompile()(implicit testPath: utest.framework.TestPath) = {
+    val testName = testPath.value.mkString("/")
+    val yaml = load(s"$testName.yaml")
+    val codegen = new Codegen(
+      LogAdapter.StdOut,
+      CodegenConfig(
+        handleErrors = true,
+        JsonLibrary.Circe
+      )
+    )
+    val result = codegen
+      .generateUnsafe(
+        yaml,
+        Some("io.github.ghostbuster91.sttp.client3.example")
+      )
+    result.syntax.shouldCompile()
+  }
+
+  private def load(fileName: String): String =
+    FileLoader.loadFile("api", fileName)
+}
