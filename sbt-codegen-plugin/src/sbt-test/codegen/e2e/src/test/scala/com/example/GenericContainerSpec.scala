c0a395a48a3295950c67d148c82b5f29885f9080
ghostbuster91 <kghost0@gmail.com>
2021-05-09 09:09:07 +0200

    Add tests for rest of the petStore api

--- sbt-codegen-plugin/src/sbt-test/codegen/e2e/src/test/scala/com/example/GenericContainerSpec.scala
+++ sbt-codegen-plugin/src/sbt-test/codegen/e2e/src/test/scala/com/example/GenericContainerSpec.scala
@@ -7,7 +7,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.wait.strategy.Wait
-import sttp.client3.HttpURLConnectionBackend
+import sttp.client3.{HttpURLConnectionBackend, Identity, SttpBackend}
+import sttp.model.StatusCode
+
+import java.io.File
 
 class GenericContainerSpec
     extends AnyFlatSpec
@@ -17,7 +20,7 @@ class GenericContainerSpec
     with DiffInstances
     with DiffMatcher {
   override val container: GenericContainer = GenericContainer(
-    "softwaremill/sttp.livestub:0.1.13",
+    "softwaremill/sttp.livestub:0.1.15",
     exposedPorts = Seq(7070),
     waitStrategy = Wait.forHttp("/__routes"),
     command = Seq("--openapi-spec", "/openapi.yaml"),
@@ -25,11 +28,14 @@ class GenericContainerSpec
       Seq(("com/example/openapi.yaml", "openapi.yaml", BindMode.READ_ONLY))
   )
 
+  lazy val petApi = new PetApi(
+    s"http://${container.containerIpAddress}:${container.mappedPort(7070)}"
+  )
+
+  val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
+
   it should "getPetById" in {
-    val petApi = new PetApi(
-      s"http://${container.containerIpAddress}:${container.mappedPort(7070)}"
-    )
-    val response = petApi.getPetById(1).send(HttpURLConnectionBackend())
+    val response = petApi.getPetById(1).send(backend)
     val pet = response.body.right.value
     pet should matchTo(
       Pet(
@@ -45,5 +51,106 @@ class GenericContainerSpec
         Some(Category(None, Some("Dogs")))
       )
     )
+  }
+
+  it should "findPetsByTags" in {
+    val response = petApi.findPetsByTags(List("tag1")).send(backend)
+    val pet = response.body.right.value
+    pet should matchTo(
+      List(
+        Pet(
+          name = "doggie",
+          tags = List(
+            Tag(None, Some("tag1"))
+          ),
+          photoUrls = List(
+            "http://random-photo-url.com"
+          ),
+          None,
+          Some(Status.Available),
+          Some(Category(None, Some("Dogs")))
+        )
+      )
+    )
+  }
+
+  it should "addPet" in {
+    val pet = Pet(
+      name = "doggie",
+      tags = List(
+        Tag(None, Some("tag1"))
+      ),
+      photoUrls = List(
+        "http://random-photo-url.com"
+      ),
+      None,
+      Some(Status.Available),
+      Some(Category(None, Some("Dogs")))
+    )
+    val response = petApi.addPet(pet).send(backend)
+    val petResponse = response.body.right.value
+    petResponse should matchTo(pet)
+  }
+
+  it should "updatePet" in {
+    val pet = Pet(
+      name = "doggie",
+      tags = List(
+        Tag(None, Some("tag1"))
+      ),
+      photoUrls = List(
+        "http://random-photo-url.com"
+      ),
+      None,
+      Some(Status.Available),
+      Some(Category(None, Some("Dogs")))
+    )
+    val response = petApi.updatePet(pet).send(backend)
+    val petResponse = response.body.right.value
+    petResponse should matchTo(pet)
+  }
+
+  it should "updatePetWithForm" in {
+    val someString = petApi.updatePetWithForm(11L, Some("doggo"), Some("qwe"))
+    val response = someString.send(backend)
+
+    response.code shouldBe StatusCode.MethodNotAllowed
+  }
+  it should "findPetByStatus" in {
+    val response =
+      petApi.findPetsByStatus(Some(Status3.Available)).send(backend)
+    val pet = response.body.right.value
+    pet should matchTo(
+      List(
+        Pet(
+          name = "doggie",
+          tags = List(
+            Tag(None, Some("tag1"))
+          ),
+          photoUrls = List(
+            "http://random-photo-url.com"
+          ),
+          None,
+          Some(Status.Available),
+          Some(Category(None, Some("Dogs")))
+        )
+      )
+    )
+  }
+  it should "uploadFile" in {
+    val response = petApi
+      .uploadFile(
+        11L,
+        Some("xyz"),
+        Some(
+          new File(
+            getClass.getClassLoader
+              .getResource("com/example/openapi.yaml")
+              .toURI
+          )
+        )
+      )
+      .send(backend)
+    response.body.right.value
   }
 }
