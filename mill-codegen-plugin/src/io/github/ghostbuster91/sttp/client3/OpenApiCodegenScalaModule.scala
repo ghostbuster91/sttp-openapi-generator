e1a8f0148d6fb475165882827481585cf0379e31
Scala Steward <scala_steward@virtuslab.com>
2023-02-08 15:42:06 +0000

    Update client3:circe, client3:core to 3.8.11

--- mill-codegen-plugin/src/io/github/ghostbuster91/sttp/client3/OpenApiCodegenScalaModule.scala
+++ mill-codegen-plugin/src/io/github/ghostbuster91/sttp/client3/OpenApiCodegenScalaModule.scala
@@ -43,12 +43,12 @@ trait OpenApiCodegenScalaModule extends ScalaModule {
   }
 
   private lazy val coreDeps =
-    Agg(ivy"com.softwaremill.sttp.client3::core::3.7.6")
+    Agg(ivy"com.softwaremill.sttp.client3::core::3.8.11")
 
   private lazy val circeDeps = Agg(
     ivy"io.circe::circe-core::0.14.3",
     ivy"io.circe::circe-parser::0.14.3",
-    ivy"com.softwaremill.sttp.client3::circe::3.7.6"
+    ivy"com.softwaremill.sttp.client3::circe::3.8.11"
   )
 
   override def generatedSources = T {
